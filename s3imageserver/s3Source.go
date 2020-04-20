9c7ce9eb08747895d4e478d4c0a0c198582510de
Derik Breedt <dbreedt@retrorabbit.co.za>
2020-04-20 16:30:15 +0200

    Normalised logging from fmt to log and fixed a race condition

--- s3imageserver/s3Source.go
+++ s3imageserver/s3Source.go
@@ -3,8 +3,10 @@ package s3imageserver
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/kr/s3"
@@ -20,17 +22,24 @@ type s3source struct {
 	S3Config
 }
 
+var s3SourceOnce sync.Once
+
 //A simple s3 image source, gets the image from s3 and presents as is
 func NewS3Source(config S3Config) *s3source {
+	s3SourceOnce.Do(func() {
+		http.DefaultClient.Timeout = 15 * time.Second
+	})
+
 	return &s3source{
 		S3Config: config,
 	}
 }
 
 func (s *s3source) GetImage(path string) ([]byte, error) {
+	log.Println("v4")
 	parts := strings.Split(path, "/")
 	reqURL := fmt.Sprintf("https://%v.s3.amazonaws.com/%v", parts[1], strings.Join(parts[2:], "/"))
-	fmt.Println("aws request url ", reqURL)
+	log.Println("aws request url ", reqURL)
 	req, reqErr := http.NewRequest("GET", reqURL, nil)
 	if reqErr != nil {
 		return nil, errors.Wrap(reqErr, "Could not create request")
@@ -41,7 +50,6 @@ func (s *s3source) GetImage(path string) ([]byte, error) {
 		AccessKey: s.S3Config.AWSAccess,
 		SecretKey: s.S3Config.AWSSecret,
 	})
-	http.DefaultClient.Timeout = 15 * time.Second
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, errors.Wrap(reqErr, "Failed to fetch")
